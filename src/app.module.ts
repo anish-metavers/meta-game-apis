5432cbd193e5857674fce085d78fa76f7cb7a5cd
sid-metaversity <siddharth@metaversitytechnologies.com>
2022-08-23 12:10:54 +0530

    Web Socket Code Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { MessageModule } from './message/message.module';
 @Module({
-    imports: [UserModule, AuthModule, ConfigModule.forRoot()],
+    imports: [UserModule, AuthModule, ConfigModule.forRoot(), MessageModule],
     controllers: [],
     providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     await DB();
     app.useGlobalPipes(new ValidationPipe({}));
-    await app.listen(3000);
+    await app.listen(3500);
 }
 
 bootstrap();
